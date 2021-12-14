f31e6caa2881f716400d656f3bf45d7437f3874f
HongDae <fholng@outlook.com>
2021-12-14 17:57:42 +0900

    git init

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,76 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "RequestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.printStartLine(request);
+        this.printHeaders(request);
+        this.printHeaderUtils(request);
+        this.printEtc(request);
+    }
+
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("[Remote 정보]");
+        System.out.println("request.getRemoteHost() = " + request.getRemoteHost());
+        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr());
+        System.out.println("request.getRemotePort() = " + request.getRemotePort());
+        System.out.println();
+
+        System.out.println("[Local 정보]");
+        System.out.println("request.getLocalName() = " + request.getLocalName());
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr());
+        System.out.println("request.getLocalPort() = " + request.getLocalPort());
+    }
+
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("Host 편의 조회");
+        System.out.println("request.getServerName() = " + request.getServerName());
+        System.out.println("request.getServerPort() = " + request.getServerPort());
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println();
+
+        System.out.println("[Cookie 편의 조회]");
+        if (request.getCookies() != null)
+            for (Cookie cookie : request.getCookies())
+                System.out.printf("%s : %s", cookie.getName(), cookie.getValue());
+
+        System.out.println();
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
+    }
+
+    private void printHeaders(HttpServletRequest request) {
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName ->
+                        System.out.printf("%s: %s", headerName, request.getHeader(headerName)));
+    }
+
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("request.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure());
+    }
+
+}
