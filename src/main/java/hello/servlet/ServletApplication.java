d83a56770e72f34f713637c4eb5c5412d5bc07bb
DaeHyeonHong <fholng@outlook.com>
2023-07-22 11:42:17 +0900

    init

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,9 +2,7 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan // Servlet 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
